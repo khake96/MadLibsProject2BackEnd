5b13bfd631506f3620bdffa69426910232851758
Oleksander <sanya953303@gmail.com>
2020-12-26 08:54:06 -0800

    servlets

---
+++ src/main/java/com/revature/web/LoginServlet.java
@@ -0,0 +1,32 @@
+package com.revature.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		res.setContentType("application/json");
+
+		res.setStatus(403); // We override it, if everything is okay
+
+		final String URI = req.getRequestURI().replace("/madlibs/", "");
+
+		switch (URI) {
+		case "login":
+			System.out.println(req.getAttributeNames());
+			System.out.println(req.getAttribute("password"));
+			System.out.println(req.getAttribute("userName"));	
+			res.setStatus(200);
+			break;
+		}
+
+	}
+}
