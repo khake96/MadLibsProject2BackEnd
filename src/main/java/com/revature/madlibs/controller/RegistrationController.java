2b139ff5a7dbdf521a4f3e4eb9747f339c149815
Kevin Hake <kevin.hake@gmail.com>
2021-01-01 15:08:42 -0500

    started fleshing out the controllers

--- src/main/java/com/revature/madlibs/controller/RegistrationController.java
+++ src/main/java/com/revature/madlibs/controller/RegistrationController.java
@@ -1,38 +1,45 @@
 package com.revature.madlibs.controller;
 
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.madlibs.models.Login;
 import com.revature.madlibs.models.User;
 import com.revature.madlibs.service.ServiceImpl;
 
 @RestController
-@RequestMapping(value="")
-@CrossOrigin // left open for now as no security concerns in dev/ops
-public class LoginController {
+@RequestMapping(value="/registration")
+@CrossOrigin
+public class RegistrationController {
 	
 	private ServiceImpl service;
 	
 	@Autowired
-	public LoginController(ServiceImpl service) {
+	public RegistrationController(ServiceImpl service) {
 		super();
 		this.service = service;
 	}
 
-	@PostMapping(value="/game")
-	public ResponseEntity<User> userLogin(@RequestBody Login login) {
-		User user = service.userLogin(login);
-		if(user.getFirst_name()!=null) {
+	@PostMapping(value="")
+	public ResponseEntity<User> insert(User user) {
+		User returnedUser = service.registerUser(user);
+		if(returnedUser != null) {
+			return ResponseEntity.status(HttpStatus.OK).body(returnedUser);
+		} else return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+	}
+
+	@GetMapping("/update")
+	public ResponseEntity<User> update(User user) {
+		User returnedUser = service.updateUser(user);
+		if(returnedUser != null) {
 			return ResponseEntity.status(HttpStatus.OK).body(user);
 		} else return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
 	}
-	
 
 }
